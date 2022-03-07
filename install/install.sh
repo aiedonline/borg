db96349e88bca901eedff9d62f3631340c2940a3
“aiedonline” <wellington.aied@gmail.com>
2022-03-07 11:58:58 -0300

    Subindo o scritp install

---
+++ install/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -r /tmp/borg
+mkdir /tmp/borg
+cp -r /opt/borg/* /tmp/borg/
+
+rm -r /tmp/borg/.git
+rm -r /tmp/borg/.server
+rm -r /tmp/borg/.client
+rm -r /tmp/borg/data
+rm -r /tmp/borg/database
+
+mkdir /tmp/borg/.server
+mkdir /tmp/borg/.client
+mkdir /tmp/borg/data
+mkdir /tmp/borg/database
+
+scp -rp /tmp/borg/* borg@54.37.137.117:/opt/borg/
+
+
