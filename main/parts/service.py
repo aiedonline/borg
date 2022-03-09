5a99efa70757f37160d866adb44d6281a3db3a90
aiedonline <wellington.aied@gmail.com>
2022-03-09 16:21:36 -0300

    Update service.py

--- main/parts/service.py
+++ main/parts/service.py
@@ -4,7 +4,9 @@ import uuid;
 class Service():
     def __init__(self):
         self.sessions = {};
-    def new(self):
+    def keynew(self):
         session_id = str(uuid.uuid4());
         self.sessions[session_id] = str(uuid.uuid4());
         return self.sessions[session_id];
+    def keyclose(self, session_id):
+        self.sessions[session_id] = None;
