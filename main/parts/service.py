800abb775996697605a0498dd625f96e7440742b
aiedonline <wellington.aied@gmail.com>
2022-03-09 16:20:02 -0300

    Update service.py

--- main/parts/service.py
+++ main/parts/service.py
@@ -1,6 +1,10 @@
-
+import uuid;
 
 
 class Service():
     def __init__(self):
-        print();
+        self.sessions = {};
+    def new(self):
+        session_id = str(uuid.uuid4());
+        self.sessions[session_id] = str(uuid.uuid4());
+        return self.sessions[session_id];
